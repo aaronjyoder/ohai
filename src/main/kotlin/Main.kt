5fecd93687d202bb7c50dc8269fdcf403b2fcaed
Aaron J Yoder <aaronjyoder@gmail.com>
2022-04-15 08:33:45 -0400

    Add app icon

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,6 +6,9 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.res.loadImageBitmap
+import androidx.compose.ui.res.useResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
@@ -25,10 +28,10 @@ fun OhaiApp() {
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "Ohai 0.1.0-alpha",
+        title = "Ohai 0.1.1-alpha",
         state = WindowState(width = 400.dp, height = 550.dp),
         resizable = false,
-//        icon = BitmapPainter(useResource("icon/icon.png", ::loadImageBitmap))
+        icon = BitmapPainter(useResource("icon/ohai_icon_512.png", ::loadImageBitmap)) // TODO: Icon looks like garbage on Windows for some reason, should fix
     ) {
         Box(modifier = Modifier.fillMaxSize().background(Color(240, 240, 240)))
         OhaiApp()
